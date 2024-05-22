36e1089c6cfe73693f3556e629291eb8b5b5de79
Michael Ballester <mike@mikebgdev.com>
2024-05-22 22:49:39 +0200

    notebook classification and documentation

--- ai_analsyis_flow_people/pages/bigdata.py
+++ ai_analsyis_flow_people/pages/bigdata.py
@@ -1,7 +1,165 @@
 import reflex as rx
+import json
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
 
 from ai_analsyis_flow_people.components.navigation import navbar
 from ai_analsyis_flow_people.components.template import template
+from ai_analsyis_flow_people.components.graphs import stat_card
+
+
+def read_data_from_json(file_path):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data
+
+
+def process_data_track(data_list):
+    df = pd.DataFrame(data_list)
+
+    mean_df = pd.DataFrame(df['mean'].tolist(), columns=[f'mean_{i}' for i in range(8)])
+    cov_df = pd.DataFrame([item for sublist in df['covariance'] for item in sublist],
+                          columns=[f'cov_{i}' for i in range(8)])
+
+    df = pd.concat([df, mean_df, cov_df], axis=1)
+
+    df.drop(['mean', 'covariance'], axis=1, inplace=True)
+
+    return df
+
+
+persons_file = './notebooks/data/proyecto_ia_arch.persons.json'
+tracks_file = './notebooks/data/proyecto_ia_arch.tracks.json'
+
+data_person_list = read_data_from_json(persons_file)
+processed_person_df = pd.DataFrame(data_person_list)
+
+data_track_list = read_data_from_json(tracks_file)
+processed_track_df = process_data_track(data_track_list)
+
+
+def format_timestamp(timestamp):
+    return pd.to_datetime(timestamp, unit='s')
+
+
+def plot_person_durations_histogram(track_df):
+    track_df['start_time'] = format_timestamp(track_df['start_time'])
+    track_df['end_time'] = format_timestamp(track_df['end_time'])
+    track_df['duration'] = track_df['end_time'] - track_df['start_time']
+    fig = px.histogram(track_df, x='duration', nbins=20)
+    fig.update_layout(title='Histograma de Duraciones de Tracks', xaxis_title='Duración del Track',
+                      yaxis_title='Frecuencia')
+    return fig
+
+
+def plot_person_durations_over_time(track_df):
+    track_df['start_time'] = format_timestamp(track_df['start_time'])
+    track_df['end_time'] = format_timestamp(track_df['end_time'])
+    track_df['duration'] = track_df['end_time'] - track_df['start_time']
+    track_df = track_df.sort_values(by='start_time')
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=track_df['start_time'], y=track_df['duration'], mode='lines+markers'))
+    fig.update_layout(title='Duración de Tracks a lo largo del tiempo', xaxis_title='Tiempo de inicio',
+                      yaxis_title='Duración del Track')
+    return fig
+
+
+def plot_person_start_vs_end(track_df):
+    fig = go.Figure()
+    fig.add_trace(
+        go.Scatter(x=track_df['start_time'], y=track_df['end_time'], mode='markers', marker=dict(color='orange')))
+    fig.update_layout(title='Tiempo de inicio vs Tiempo de fin de los Tracks', xaxis_title='Tiempo de inicio',
+                      yaxis_title='Tiempo de fin')
+    return fig
+
+
+def plot_track_confidence_vs_time(track_df):
+    track_df['time'] = format_timestamp(track_df['time'])
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=track_df['time'], y=track_df['conf'], mode='lines+markers'))
+    fig.update_layout(title='Confianza del Track a lo largo del tiempo', xaxis_title='Tiempo', yaxis_title='Confianza')
+    return fig
+
+
+def plot_track_position(track_df):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=track_df['cord_x'], y=track_df['cord_y'], mode='markers'))
+    fig.update_layout(title='Posición de los Tracks', xaxis_title='Coordenada X', yaxis_title='Coordenada Y')
+    return fig
+
+
+def generate_pie_chart(track_df):
+    fig = px.pie(track_df, names='state', title='Distribución de Tracks por Estado')
+    return fig
+
+
+def prepare_data_stat_card():
+    total_persons = int(processed_person_df['track_id'].count())
+    total_tracks = int(processed_track_df['track_id'].count())
+    average_confidence = round(processed_track_df['conf'].mean(), 2)
+    active_tracks = int(processed_track_df[processed_track_df['state'] == 1]['track_id'].count())
+    average_track_duration = round((processed_track_df['time'].max() - processed_track_df['time'].min()) / total_tracks,
+                                   2)
+    average_hits_per_track = round(processed_track_df['hits'].mean(), 2)
+
+    return [
+        ["Total Persons", total_persons],
+        ["Total Tracks", total_tracks],
+        ["Active Tracks", active_tracks],
+        ["Average Confidence", average_confidence],
+        ["Average Track Duration", average_track_duration],
+        ["Average Hits per Track", average_hits_per_track]
+    ]
+
+
+def content_grid():
+    return rx.chakra.grid(
+        *[
+            rx.chakra.grid_item(stat_card(*c), col_span=1, row_span=1)
+            for c in prepare_data_stat_card()
+        ],
+        rx.chakra.grid_item(
+            rx.heading("Duración del seguimiento de personas"),
+            rx.plotly(data=plot_person_durations_histogram(processed_person_df)),
+            col_span=3,
+            row_span=2,
+        ),
+        rx.chakra.grid_item(
+            rx.heading("Duración de Tracks a lo largo del tiempo"),
+            rx.plotly(data=plot_person_durations_over_time(processed_person_df)),
+            col_span=3,
+            row_span=2,
+        ),
+        rx.chakra.grid_item(
+            rx.heading("Tiempo de inicio vs Tiempo de fin de los Tracks"),
+            rx.plotly(data=plot_person_start_vs_end(processed_person_df)),
+            col_span=3,
+            row_span=2,
+        ),
+        rx.chakra.grid_item(
+            rx.heading("Confianza del Track a lo largo del tiempo"),
+            rx.plotly(data=plot_track_confidence_vs_time(processed_track_df)),
+            col_span=3,
+            row_span=2,
+        ),
+        rx.chakra.grid_item(
+            rx.heading("Posición de los Tracks"),
+            rx.plotly(data=plot_track_position(processed_track_df)),
+            col_span=3,
+            row_span=2,
+        ),
+        rx.chakra.grid_item(
+            rx.heading("Distribución de Tracks por Estado"),
+            rx.plotly(data=generate_pie_chart(processed_track_df)),
+            col_span=3,
+            row_span=2,
+        ),
+        template_columns="repeat(6, 1fr)",
+        width="100%",
+        gap=6,
+        row_gap=8,
+    )
 
 
 @template
@@ -9,7 +167,7 @@ def bigdata() -> rx.Component:
     return rx.box(
         navbar(heading="Dashboard Big Data"),
         rx.box(
-            rx.text("placeholder"),
+            content_grid(),
             background_color=rx.color("mauve", 2),
             padding="2em",
             min_height="calc(100vh - calc(50px + 2em))",
--- ai_analsyis_flow_people/pages/notebooks.py
+++ ai_analsyis_flow_people/pages/notebooks.py
@@ -4,16 +4,72 @@ from ai_analsyis_flow_people.components.navigation import navbar
 from ai_analsyis_flow_people.components.template import template
 
 
+def content():
+    return rx.box(
+        rx.section(
+            rx.heading("Análisis Exploratorio de Datos (EDA)", level=1),
+            rx.hstack(
+                rx.image(src="/img/modelos/distribucion_cordx.png", alt="Distribución Cord X", width="auto", max_width="30%"),
+                rx.image(src="/img/modelos/distribucion_cordy.png", alt="Distribución Cord Y", width="auto", max_width="30%"),
+                rx.image(src="/img/modelos/relacion_cord.png", alt="Relación Cord XY", width="auto", max_width="30%"),
+                spacing="2",
+            ),
+            margin_bottom="2em",
+        ),
+        rx.section(
+            rx.heading("Clustering", level=1),
+            rx.section(
+                rx.heading("K-Means Clustering", level=2),
+                rx.hstack(
+                    rx.image(src="/img/modelos/elbow.png", alt="Elbow Method K-Means", width="auto", max_width="50%"),
+                    rx.image(src="/img/modelos/kmeans.png", alt="K-Means Clustering", width="auto", max_width="50%"),
+                    spacing="2",
+                ),
+                rx.text("Silhouette Score para K-Means: 0.5872"),
+                margin_bottom="2em",
+            ),
+            rx.section(
+                rx.heading("DBSCAN Clustering", level=2),
+                rx.hstack(
+                    rx.image(src="/img/modelos/dbscan.png", alt="DBSCAN Clustering", width="auto", max_width="50%"),
+                    spacing="2",
+                ),
+                rx.text("Silhouette Score para DBSCAN: 0.6451"),
+                margin_bottom="2em",
+            ),
+        ),
+        rx.section(
+            rx.heading("Classification", level=1),
+            rx.section(
+                rx.heading("Árbol de Decisión", level=2),
+                rx.hstack(
+                    rx.image(src="/img/modelos/decision_tree.png", alt="Decision tree", width="auto", max_width="100%"),
+                    spacing="2",
+                ),
+                rx.text("Accuracy del Árbol de Decisión: 0.90"),
+                margin_bottom="2em",
+            ),
+            rx.section(
+                rx.heading("Random Forest", level=2),
+                rx.hstack(
+                    rx.image(src="/img/modelos/random_forest.png", alt="Random Forest", width="auto", max_width="50%"),
+                    spacing="2",
+                ),
+                rx.text("Accuracy del Bosque Aleatorio: 0.95"),
+                margin_bottom="2em",
+            ),
+        ),
+        background_color=rx.color("mauve", 2),
+        padding="2em",
+        min_height="calc(100vh - 50px - 2em)",
+    )
+
+
 @template
 def notebooks() -> rx.Component:
     return rx.box(
-        navbar(heading="Notebooks"),
-        rx.box(
-            rx.text("placeholder"),
-            background_color=rx.color("mauve", 2),
-            padding="2em",
-            min_height="calc(100vh - calc(50px + 2em))",
-        ),
+        navbar(heading="Resultados de los Modelos"),
+        content(),
         padding_top="calc(50px + 2em)",
         padding_left="250px",
     )
